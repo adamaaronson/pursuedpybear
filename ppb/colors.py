83dd3a422d8c689d4b0db1e081155691a3b635cb
Adam Aaronson <adamaaronson@gmail.com>
2024-05-20 15:26:16 -0400

    Add color constants and rgb number validation

--- ppb/colors.py
+++ ppb/colors.py
@@ -5,9 +5,26 @@ class Color():
     blue: int
 
     def __init__(self, red: int, green: int, blue: int):
+        for key, value in {'red': red, 'green': green, 'blue': blue}.items():
+            if value < 0:
+                raise ValueError(f'{key} cannot be less than 0.')
+            elif value > 255:
+                raise ValueError(f'{key} cannot be greater than 255.')
+
         self.red = red
         self.green = green
         self.blue = blue
     
     def __repr__(self) -> str:
         return f'<{type(self).__name__} red={self.red} green={self.green} blue={self.blue}>'
+
+
+BLACK = Color(0, 0, 0)
+WHITE = Color(255, 255, 255)
+GRAY = Color(127, 127, 127)
+RED = Color(255, 0, 0)
+GREEN = Color(0, 255, 0)
+BLUE = Color(0, 0, 255)
+CYAN = Color(0, 255, 255)
+MAGENTA = Color(255, 0, 255)
+YELLOW = Color(255, 255, 0)
