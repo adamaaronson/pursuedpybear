3766ddacc9987a5d1c00d93bcc298c670edecd1f
Adam Aaronson <adamaaronson@gmail.com>
2024-05-20 16:26:08 -0400

    Fix color validation using post_init

--- ppb/colors.py
+++ ppb/colors.py
@@ -1,22 +1,18 @@
 from dataclasses import dataclass
 
-@dataclass(frozen=True)
+@dataclass()
 class Color():
     """An RGB color."""
     red: int
     green: int
     blue: int
 
-    def __init__(self, red: int, green: int, blue: int):
-        for key, value in {'red': red, 'green': green, 'blue': blue}.items():
+    def __post_init__(self):
+        for key, value in {'red': self.red, 'green': self.green, 'blue': self.blue}.items():
             if value < 0:
                 raise ValueError(f'{key} cannot be less than 0.')
             elif value > 255:
                 raise ValueError(f'{key} cannot be greater than 255.')
-
-        self.red = red
-        self.green = green
-        self.blue = blue
     
     def __iter__(self) -> tuple[int]:
         return (self.red, self.green, self.blue)
